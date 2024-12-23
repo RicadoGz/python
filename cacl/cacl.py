23a2d08f1ccd58d944f935c20479530dae4fcee3
RicardoGz <163072550+RicadoGz@users.noreply.github.com>
2024-12-23 13:15:25 -0500

    Add cacl.py it show how many interview I got

---
+++ cacl/cacl.py
@@ -0,0 +1,6 @@
+applyJob=100
+print(f"I already apply {applyJob} job ")
+automaticReject=99
+myInterview = applyJob - automaticReject
+print(f"after reject {automaticReject} I got {myInterview} intervew")
+
