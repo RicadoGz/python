e41b677dfe1bddfb1e10471b1a2ae004e5af17e8
RicardoGz <163072550+RicadoGz@users.noreply.github.com>
2024-12-23 17:04:46 -0500

    add type function

---
+++ type-and-transfer/type.py
@@ -0,0 +1,13 @@
+intNumber=10
+charValue='c'
+stringValue="hello"
+doubleValue=5.333
+print(type(intNumber),intNumber)
+print(type(charValue),charValue)
+print(type(stringValue),stringValue)
+print(type (doubleValue),doubleValue)
+
+strIntNumber=str(intNumber)
+intDoubleValue=int(doubleValue)
+print(type(strIntNumber),strIntNumber)
+print(type (intDoubleValue),intDoubleValue)
